bd8b67f0530a2a688df5133e37058eda89d10c52
KM127PL <km127pl2@gmail.com>
2022-11-06 14:42:06 +0100

    actually, add everything

--- assets/ts/theme.ts
+++ assets/ts/theme.ts
@@ -1,30 +1,30 @@
 
 function setTheme(theme: string) {
-    // update the body
-    const body : HTMLElement = document.getElementsByTagName('body')[0];
-    body.classList.remove('theme-light', 'theme-dark');
-    body.classList.add(`theme-${theme}`);
+	// update the body
+	const body: HTMLElement = document.getElementsByTagName('body')[0];
+	body.classList.remove('theme-light', 'theme-dark');
+	body.classList.add(`theme-${theme}`);
 
-    // update the button
-    const button : HTMLElement = document.getElementById('theme-icon');
+	// update the button
+	const button: HTMLElement = document.getElementById('theme-icon');
 
-    if (button) { 
-        button.classList.remove('fa-sun', 'fa-moon');
-        button.classList.add(theme === 'light' ? 'fa-moon' : 'fa-sun');
-    }
+	if (button) {
+		button.classList.remove('fa-sun', 'fa-moon');
+		button.classList.add(theme === 'light' ? 'fa-moon' : 'fa-sun');
+	}
 
-    // update local storage
-    localStorage.setItem('theme', theme);
+	// update local storage
+	localStorage.setItem('theme', theme);
 }
 
 function toggleTheme() {
-    const theme = localStorage.getItem('theme') || 'light';
-    setTheme(theme === 'light' ? 'dark' : 'light');
+	const theme = localStorage.getItem('theme') || 'light';
+	setTheme(theme === 'light' ? 'dark' : 'light');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const theme = localStorage.getItem('theme') || 'light';
-    setTheme(theme);
+	const theme = localStorage.getItem('theme') || 'light';
+	setTheme(theme);
 });
 
 // Path: src\index.ts
