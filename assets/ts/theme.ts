c660c1cad32479030dda6d7af1e16e90bd9b5d81
KM127PL <km127pl2@gmail.com>
2022-11-08 18:35:54 +0100

    Update css to use sass

--- assets/ts/theme.ts
+++ assets/ts/theme.ts
@@ -1,27 +1,3 @@
-
-// function setTheme(theme: string) {
-// 	// update the body
-// 	const body: HTMLElement = document.getElementsByTagName('body')[0];
-// 	body.classList.remove('theme-light', 'theme-dark');
-// 	body.classList.add(`theme-${theme}`);
-
-// 	// update the button
-// 	const button: HTMLElement = document.getElementById('theme-icon');
-
-// 	if (button) {
-// 		button.classList.remove('fa-sun', 'fa-moon');
-// 		button.classList.add(theme === 'light' ? 'fa-moon' : 'fa-sun');
-// 	}
-
-// 	// update local storage
-// 	localStorage.setItem('theme', theme);
-// }
-
-// function toggleTheme() {
-// 	const theme = localStorage.getItem('theme') || 'light';
-// 	setTheme(theme === 'light' ? 'dark' : 'light');
-// }
-
 document.addEventListener('DOMContentLoaded', () => {
 	UpdateRandomColor();
 });
@@ -32,11 +8,11 @@ document.addEventListener("keydown", (ev: KeyboardEvent) => {
 	}
 })
 
-function UpdateRandomColor() : String {
+function UpdateRandomColor(): String {
 	let colors: String[] = ["amaranth-purple", "cinnammon-satin", "green", "cyan-process", "fuchsia", "lavender-floral", "middle-red", "bittersweet", "persimmon", "cosmic-cobalt"];
 	let randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-	const root : HTMLElement = document.documentElement;
+	const root: HTMLElement = document.documentElement;
 	root.style.setProperty("--color", `var(--${randomColor})`)
 
 	console.log(`Setting random color to ${randomColor}`);
@@ -44,8 +20,8 @@ function UpdateRandomColor() : String {
 	return randomColor;
 }
 
-function GetCurrentColor() : String {
-	const root : HTMLElement = document.documentElement;
+function GetCurrentColor(): String {
+	const root: HTMLElement = document.documentElement;
 	return getComputedStyle(root).getPropertyValue("--color");
 }
 
